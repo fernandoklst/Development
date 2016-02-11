6257d792ff589c5fe11677cb2312fdc52c19dbee
fernandoklst <fernando@klstinc.com>
2016-02-10 20:41:18 -0500

    email app

---
+++ KLST.TataCommunications.Email/KLST.TataCommunications.Email/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KLST.TataCommunications.Email.Startup))]
+namespace KLST.TataCommunications.Email
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
